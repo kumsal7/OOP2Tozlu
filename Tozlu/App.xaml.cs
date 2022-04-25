78d3466abebaa9a1ea3bd5868fac48a0d05093e6
kumsal7 <73231366+kumsal7@users.noreply.github.com>
2022-04-25 18:01:56 +0300

    Add files via upload

---
+++ Tozlu/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Tozlu.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace Tozlu
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new HomePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
