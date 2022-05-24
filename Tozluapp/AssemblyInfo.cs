6793711537a587a7df0d2f49e887b21cf92c2de5
kumsal7 <73231366+kumsal7@users.noreply.github.com>
2022-05-24 10:30:11 +0300

    Add files via upload

---
+++ Tozluapp/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly:ExportFont("PlayfairDisplay-Black.ttf", Alias ="CabinSketch")]
+[assembly:ExportFont("PlayfairDisplay-Bold.ttf", Alias ="FontAwesome")]
+
---
+++ Tozluapp/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Tozluapp.Model;
+using System.Linq;
+
+
+namespace Tozluapp.Services
+{
+    public class UserService
+    {
+        FirebaseClient client;
+        public UserService()
+        {
+            client = new FirebaseClient("https://tozluapp-default-rtdb.firebaseio.com/");
+        }
+        public async Task<bool> IsUserExists(string uname)
+        {
+            var user = (await client.Child("Users")
+                .OnceAsync<User>()).Where(u => u.Object.Username == uname).FirstOrDefault();
+            return (user != null);
+        }
+    }
+}
