b342a2fe5354527c38de1212d22b442ec3322e7a
kumsal7 <73231366+kumsal7@users.noreply.github.com>
2022-06-12 22:13:01 +0300

    documents commit

---
+++ eshop-mobile-client-main/eShopOnContainers/eShopOnContainers.Core/Views/HomePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using eShopOnContainers.Core.ViewModels;
+
+namespace eShopOnContainers.Core.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            List<Image> images = new List<Image>()
+            {
+                new Image(){Source="https://img.tozlu.com/Uploads/SayfaTasarim/Sayfa10/Mutluluk-Zamani-6afd.jpg"},
+                new Image(){Source="https://img.tozlu.com/Uploads/SayfaTasarim/Sayfa10/Yeni-Sezon-Ilk-Firsatlari-7808.jpg"},
+                new Image(){Source="https://img.tozlu.com/Uploads/SayfaTasarim/Sayfa10/Ilk-Bahar-Yaz-Koleksiyonu-8529.jpg"}
+            };
+            HomePageCarousel.ItemsSource = images;
+            }
+    }
+}
